8a175187714fca03c7ad3338fd9f1768ac79e596
Ediel Queiroz <ediel.rp@gmail.com>
2022-06-06 19:59:01 -0300

    First kotlin project commit

---
+++ pokemon-exp/src/main/kotlin/com/pokemon/pokemonexp/PokemonExpApplication.kt
@@ -0,0 +1,11 @@
+package com.pokemon.pokemonexp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PokemonExpApplication
+
+fun main(args: Array<String>) {
+	runApplication<PokemonExpApplication>(*args)
+}
---
+++ pokemon-exp/src/test/kotlin/com/pokemon/pokemonexp/PokemonExpApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pokemon.pokemonexp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PokemonExpApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
